496ffac534d55146b56f7187e939f2a95cdb7228
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-04 00:23:44 +0430

    Init Express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8000);
